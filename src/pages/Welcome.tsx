c883a4b710127ed1e109cd1ed20ebda9f18b16c0
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-04-22 04:26:13 -0300

    Create the welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,12 +1,69 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, SafeAreaView, Image, TouchableOpacity, StyleSheet } from 'react-native';
+
+import wateringImg from '../assets/watering.png';
+import colors from '../styles/colors';
 
 export function Welcome() {
     return (
-        <View>
-            <Text>
-                Gerencie suas plantas de forma fácil
+        <SafeAreaView style={style.container}>
+            <Text style={style.title}>
+                Gerencie{'\n'}
+                suas plantas{'\n'}
+                de forma fácil
             </Text>
-        </View>
+
+            <Image source={wateringImg} style={style.image} />
+
+            <Text style={style.subtitle}>
+                Não esqueça mais de regar suas plantas.{'\n'}
+                Nós cuidamos de lembrar você sempre que{'\n'}
+                precisar
+            </Text>
+
+            <TouchableOpacity style={style.button} activeOpacity={0.6}>
+                <Text style={style.buttonText}>
+                    Avançar
+                </Text>
+            </TouchableOpacity>
+        </SafeAreaView>
     )
 }
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading,
+        marginTop: 60
+    },
+    subtitle: {
+        textAlign: 'center',
+        fontSize: 18,
+        paddingHorizontal: 20,
+        color: colors.heading
+    },
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        paddingHorizontal: 10
+    },
+    image: {
+        height: 284,
+        width: 292
+    },
+    buttonText: {
+        color: colors.white,
+        fontSize: 24
+    }
+})
