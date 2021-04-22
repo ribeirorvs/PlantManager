b34aad7ba54cf12bec3189c9f84e5fe7e4e3afb7
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-04-22 03:48:53 -0300

    Create src folder and welcome page

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export function Welcome() {
+    return (
+        <View>
+            <Text>
+                Gerencie suas plantas de forma fácil
+            </Text>
+        </View>
+    )
+}
