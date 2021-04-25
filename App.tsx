b3014f5d6b77e3385ccbbc451a67c40147b8b627
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-04-25 13:37:01 -0300

    Including font Jost from google fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import { Welcome } from './src/pages/Welcome';
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
   return (
     <Welcome />
   )
