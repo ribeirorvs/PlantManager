9f5cfc2088c01e67ad1c89640df304984d2dfc40
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-04-25 16:18:41 -0300

    include button component and change the behavior of input

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -1,18 +1,54 @@
 import content from '*.png';
-import React from 'react';
-import { SafeAreaView, StyleSheet, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    View,
+    Text,
+    TextInput,
+    KeyboardAvoidingView,
+} from 'react-native';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+import { Button } from '../components/Button';
 
 export function UserIdentification() {
+    const [isFocused, setIsFocused] = useState(false);
+    function handleInputBlur() {
+        setIsFocused(false);
+    }
+
+    function handleInputFocus() {
+        setIsFocused(true);
+    }
     return (
-        < SafeAreaView style={style.container}>
-            < View style={style.content}>
-                < View style={style.form}>
-                    < Text style={style.emoji}>
-                        :D
+        <KeyboardAvoidingView style={style.container} >
+            < SafeAreaView style={style.container}>
+                < View style={style.content}>
+                    < View style={style.form}>
+                        < Text style={style.emoji}>
+                            :)
                     </Text>
+                        < Text style={style.title}>
+                            Como podemos{'\n'}
+                        chamar você?
+                    </Text>
+                        < TextInput
+                            style={[
+                                style.input,
+                                isFocused && { borderColor: colors.green }
+                            ]}
+                            placeholder="Digite um nome"
+                            onBlur={handleInputBlur}
+                            onFocus={handleInputFocus}
+                        />
+                        < View style={style.footer}>
+                            < Button />
+                        </View>
+                    </View>
                 </View>
-            </View>
-        </SafeAreaView>
+            </SafeAreaView>
+        </KeyboardAvoidingView>
     )
 }
 const style = StyleSheet.create({
@@ -30,9 +66,34 @@ const style = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         paddingHorizontal: 54,
-        alignItems: 'center'
+        alignItems: 'center',
     },
     emoji: {
+        fontSize: 44,
+        color: colors.green
+    },
+    input: {
+        borderBottomWidth: 1,
+        borderColor: colors.gray,
+        color: colors.heading,
+        width: '100%',
+        fontSize: 18,
+        marginTop: 50,
+        padding: 10,
+        textAlign: 'center'
+    },
+    title: {
+        fontSize: 24,
+        lineHeight: 32,
+        textAlign: 'center',
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        marginTop: 20
+    },
+    footer: {
+        width: '100%',
+        marginTop: 40,
+        paddingHorizontal: 20
 
     }
 })
