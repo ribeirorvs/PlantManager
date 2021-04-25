551f6331822ad6bd0e0d4d11ed9dec46baee818d
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-04-25 15:29:25 -0300

    Correct page layout

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -5,7 +5,8 @@ import {
     Image,
     TouchableOpacity,
     StyleSheet,
-    Dimensions
+    Dimensions,
+    View
 } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import fonts from '../styles/fonts';
@@ -16,32 +17,34 @@ import colors from '../styles/colors';
 export function Welcome() {
     return (
         <SafeAreaView style={style.container}>
-            <Text style={style.title}>
-                Gerencie{'\n'}
-                suas plantas{'\n'}
-                de forma fácil
-            </Text>
-
-            <Image
-                source={wateringImg}
-                style={style.image}
-                resizeMode='contain'
-            />
-
-            <Text style={style.subtitle}>
-                Não esqueça mais de regar suas plantas.{'\n'}
-                Nós cuidamos de lembrar você sempre que{'\n'}
-                precisar
-            </Text>
-
-            <TouchableOpacity style={style.button} activeOpacity={0.6}>
-                <Text>
-                    <Feather
-                        name='chevron-right'
-                        style={style.buttonIcon}
-                    />
+            < View style={style.wrapper}>
+                <Text style={style.title}>
+                    Gerencie{'\n'}
+                    suas plantas{'\n'}
+                    de forma fácil
                 </Text>
-            </TouchableOpacity>
+
+                <Image
+                    source={wateringImg}
+                    style={style.image}
+                    resizeMode='contain'
+                />
+
+                <Text style={style.subtitle}>
+                    Não esqueça mais de regar suas plantas.{'\n'}
+                    Nós cuidamos de lembrar você sempre que{'\n'}
+                    precisar
+                </Text>
+
+                <TouchableOpacity style={style.button} activeOpacity={0.6}>
+                    <Text>
+                        <Feather
+                            name='chevron-right'
+                            style={style.buttonIcon}
+                        />
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
@@ -49,8 +52,6 @@ export function Welcome() {
 const style = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-around'
     },
     title: {
         fontSize: 32,
@@ -82,5 +83,11 @@ const style = StyleSheet.create({
     buttonIcon: {
         color: colors.white,
         fontSize: 32
+    },
+    wrapper: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        paddingHorizontal: 20
     }
 })
