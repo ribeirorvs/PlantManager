609313e95b35f4cfaca15b6c38aafe058a4a34a3
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-04-25 14:51:49 -0300

    add font Jost and app font loading

--- App.tsx
+++ App.tsx
@@ -5,13 +5,18 @@ import {
   Jost_400Regular,
   Jost_600SemiBold
 } from '@expo-google-fonts/jost';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
-
   const [fontsLoaded] = useFonts({
     Jost_400Regular,
     Jost_600SemiBold
   });
+
+  if (!fontsLoaded)
+    return (
+      <AppLoading />
+    )
 
   return (
     <Welcome />
