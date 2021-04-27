78dbef16d8b72833c2a308028e6f5e4cb5132c14
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-04-27 02:38:46 -0300

    Correct some details in the confirmation page style

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -11,52 +11,60 @@ import fonts from '../styles/fonts';
 
 export function Confirmation() {
     return (
-        < SafeAreaView style={style.container} >
-            < View style={style.content}>
-                < Text style={style.emoji} >
+        < SafeAreaView style={styles.container} >
+            < View style={styles.content}>
+                < Text style={styles.emoji} >
                     :)
                 </Text>
-                < Text style={style.title} >
+                < Text style={styles.title} >
                     Prontinho
                 </Text>
-                < Text style={sytle.subtitle} >
+                < Text style={styles.subtitle} >
                     Agora vamos começar a cuidar das suas{'\n'}
                     plantinhas com muito cuidado.
                 </Text>
-                < View style={style.footer}>
+                < View style={styles.footer}>
                     < Button />
                 </View>
             </View>
         </SafeAreaView>
     )
-
-    const style = StyleSheet.create({
-        container: {
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'space-around'
-        },
-        title: {
-            fontSize: 22,
-            fontFamily: fonts.heading,
-            textAlign: 'center',
-            color: colors.heading,
-            lineHeight: 38,
-            marginTop: 15
-        },
-        subtitle: {
-            fontFamily: fonts.text,
-            textAlign: 'center',
-            fontSize: 17,
-            paddingVertical: 20,
-            color: colors.heading
-        },
-        emoji: {
-            fontSize: 32
-        },
-        footer: {
-            width: '100%',
-            paddingHorizontal: 75
-        }
-    })
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    },
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        padding: 30
+    },
+    title: {
+        fontSize: 22,
+        fontFamily: fonts.heading,
+        textAlign: 'center',
+        color: colors.heading,
+        lineHeight: 38,
+        marginTop: 15
+    },
+    subtitle: {
+        fontFamily: fonts.text,
+        textAlign: 'center',
+        fontSize: 17,
+        paddingVertical: 10,
+        color: colors.heading
+    },
+    emoji: {
+        fontSize: 78
+    },
+    footer: {
+        width: '100%',
+        marginTop: 30,
+        paddingHorizontal: 50
+    }
+})
