4f92f0564a7c9abc30697bf5a0d0999c8808ed62
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-04-29 06:03:33 -0300

    includ svg image

--- src/components/PlantCardPrimary.tsx
+++ src/components/PlantCardPrimary.tsx
@@ -7,6 +7,7 @@ import {
     RectButton,
     RectButtonProps
 } from 'react-native-gesture-handler';
+import { SvgFromUri } from 'react-native-svg';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
@@ -23,6 +24,11 @@ export const PlantCardPrimary = ({ data, ...rest }: PlantProps) => {
             style={styles.container}
             {...rest}
         >
+            < SvgFromUri
+                uri={data.photo}
+                width={70}
+                height={70}
+            />
             < Text style={styles.text}>
                 {data.name}
             </Text>
