f4184645907574b869c06cd5e27447c7a47ab438
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-04-29 04:39:38 -0300

    Create button using rectbutton

--- src/components/EnvironmentButton.tsx
+++ src/components/EnvironmentButton.tsx
@@ -1,15 +1,43 @@
 import React from 'react';
 import {
-    View,
+    Text,
     StyleSheet
 } from 'react-native';
+import {
+    RectButton,
+    RectButtonProps
+} from 'react-native-gesture-handler';
 
-export function EnvironmentButton() {
+import colors from '../styles/colors';
+import font from '../styles/fonts';
+
+interface EnvironmentButtonProps extends RectButtonProps {
+    title: string;
+    active?: boolean;
+}
+
+export function EnvironmentButton({
+    title,
+    active = false,
+    ...rest
+}: EnvironmentButtonProps) {
     return (
-        < View ></View>
+        < RectButton
+            style={styles.container}
+            {...rest}
+        >
+            < Text style={styles.text}>
+                {title}
+            </Text>
+        </ RectButton>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
 
+    },
+    text: {
+
+    }
 })
