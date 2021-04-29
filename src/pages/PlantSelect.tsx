34f3a222c0e35525274e85c3ff73ea628d2fccd8
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-04-29 04:29:14 -0300

    Define styles for header text

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
 import colors from '../styles/colors';
+import { Header } from '../components/Header';
+import fonts from '../styles/fonts';
 
 export function PlantSelect() {
     return (
         <SafeAreaView style={styles.container}>
-            < View >
-                < Text >
-                </Text>
+            < View style={styles.header}>
+                < Header />
+                < Text style={styles.title}>
+                    Em qual hambiente
+            </Text>
+                < Text style={styles.subtitle}>
+                    você quer colocar sua planta?
+            </Text>
             </View>
         </SafeAreaView>
     )
@@ -16,6 +23,22 @@ export function PlantSelect() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: colors.background
+        backgroundColor: colors.background,
+    },
+    title: {
+        fontSize: 17,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        lineHeight: 20,
+        marginTop: 15
+    },
+    subtitle: {
+        fontSize: 17,
+        fontFamily: fonts.text,
+        color: colors.heading,
+        lineHeight: 20
+    },
+    header: {
+        paddingHorizontal: 30
     }
 })
