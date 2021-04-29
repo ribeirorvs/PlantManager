b78cf5e4883396fe8537685481d9faf7d63cfba8
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-04-29 03:50:41 -0300

    Include onpress function to plant select

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -5,11 +5,18 @@ import {
     View,
     Text
 } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
 import { Button } from '../components/Button';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function Confirmation() {
+    const navigation = useNavigation();
+
+    function handleMoveOn() {
+        navigation.navigate('PlantSelect');
+    }
+
     return (
         < SafeAreaView style={styles.container} >
             < View style={styles.content}>
@@ -26,6 +33,7 @@ export function Confirmation() {
                 < View style={styles.footer}>
                     < Button
                         title="Começar"
+                        onPress={handleMoveOn}
                     />
                 </View>
             </View>
