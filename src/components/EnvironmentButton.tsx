9e8377e02c8f8cdfa008add3a131b95498f4db57
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-04-29 04:31:49 -0300

    Create environment buttons component

---
+++ src/components/EnvironmentButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    View,
+    StyleSheet
+} from 'react-native';
+
+export function EnvironmentButton() {
+    return (
+        < View ></View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
