0a68f238cac912c5fb387e1a92d5aec5881c3790
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-05-13 21:45:29 -0300

    Add async storage to store the user name

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -15,6 +15,7 @@ import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 import { Button } from '../components/Button';
 import { useNavigation } from '@react-navigation/core';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export function UserIdentification() {
     const [isFocused, setIsFocused] = useState(false);
@@ -36,10 +37,13 @@ export function UserIdentification() {
         setName(value);
     }
 
-    function handleSubmit() {
+    async function handleSubmit() {
         if (!name) {
             return Alert.alert('Me diz como chamar você :(')
         }
+
+        await AsyncStorage.setItem('@plantmanager:user', name);
+
         navigation.navigate('Confirmation')
     }
 
