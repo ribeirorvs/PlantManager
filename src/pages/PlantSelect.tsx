1789ce1d7575799a66bf732fec0f583ea063e3d3
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-05-15 01:52:35 -0300

    Refactore the plant props

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -15,23 +15,11 @@ import api from '../services/api';
 import { PlantCardPrimary } from '../components/PlantCardPrimary';
 import { Load } from '../components/Load';
 import { useNavigation } from '@react-navigation/core';
+import { PlantProps } from '../libs/storage';
 
 interface EnvironmentProps {
     key: string;
     title: string;
-}
-
-interface PlantProps {
-    id: string,
-    name: string,
-    about: string,
-    water_tips: string,
-    photo: string,
-    environments: [string],
-    frequency: {
-        times: number,
-        repeat_every: string
-    }
 }
 
 export function PlantSelect() {
