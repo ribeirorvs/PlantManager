637ab3bfd11ef6c03004be1ce67d9bf1a26b9264
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-05-15 01:55:50 -0300

    Create file to handle plant props and storage

---
+++ src/libs/storage.ts
@@ -0,0 +1,43 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { format } from 'date-fns';
+
+export interface PlantProps {
+    id: string,
+    name: string,
+    about: string,
+    water_tips: string,
+    photo: string,
+    environments: [string],
+    frequency: {
+        times: number,
+        repeat_every: string
+    },
+    dateTimeNotification: Date;
+}
+
+export interface StoragePlantProps {
+    [id: string]: {
+        data: PlantProps;
+    }
+}
+
+export async function savePlant(plant: PlantProps): Promise<void> {
+    try {
+        const data = await AsyncStorage.getItem('@plantmanager:plants');
+        const oldPlants = data ? (JSON.parse(data) as StoragePlantProps) : {};
+
+        const newPlant = {
+            [plant.id]: {
+                data: plant
+            }
+        }
+
+        await AsyncStorage.setItem('@plantmanager:plants',
+            JSON.stringify({
+                ...newPlant,
+                ...oldPlants
+            }));
+    } catch (error) {
+        throw new Error(error);
+    }
+}
