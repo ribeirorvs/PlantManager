aa7a35160e7e60bb84b11eafe572e0df88a9576d
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-05-15 00:28:17 -0300

    Defining the styles

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -12,11 +12,13 @@ import {
 import { SvgFromUri } from 'react-native-svg';
 import waterdrop from '../assets/waterdrop.png';
 import { Button } from '../components/Button';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function PlantSave() {
     return (
-        <>
-            <View style={styles.container}>
+        <View style={styles.container}>
+            <View style={styles.plantInfo}>
                 <SvgFromUri
                     uri=""
                     height={150}
@@ -40,7 +42,7 @@ export function PlantSave() {
                         source={waterdrop}
                         style={styles.tipImage}
                     />
-                    <Text style={styles.tipText}></Text>
+                    <Text style={styles.tipText}> text text text </Text>
                 </View>
                 <Text style={styles.alertLabel}>
                     Escolha o melhor horário para ser lembrado:
@@ -50,16 +52,67 @@ export function PlantSave() {
                     onPress={() => { }}
                 />
             </View>
-        </>
+        </View>
     )
 }
 const styles = StyleSheet.create({
-    container: {},
-    plantName: {},
-    plantAbout: {},
-    controller: {},
-    tipContainer: {},
-    tipImage: {},
-    tipText: {},
-    alertLabel: {}
+    container: {
+        flex: 1,
+        justifyContent: 'space-between',
+        backgroundColor: colors.shape
+    },
+    plantInfo: {
+        flex: 1,
+        paddingHorizontal: 30,
+        paddingVertical: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: colors.shape
+    },
+    plantName: {
+        fontFamily: fonts.heading,
+        fontSize: 24,
+        color: colors.heading,
+        marginTop: 15
+    },
+    plantAbout: {
+        textAlign: 'center',
+        fontFamily: fonts.text,
+        color: colors.heading,
+        fontSize: 17,
+        marginTop: 10
+    },
+    controller: {
+        backgroundColor: colors.white,
+        padding: 20
+    },
+    tipContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: colors.blue_light,
+        padding: 20,
+        borderRadius: 20,
+        position: 'relative',
+        bottom: 65
+    },
+    tipImage: {
+        height: 56,
+        width: 56
+    },
+    tipText: {
+        flex: 1,
+        marginLeft: 20,
+        fontFamily: fonts.text,
+        color: colors.blue,
+        fontSize: 17,
+        textAlign: 'justify'
+    },
+    alertLabel: {
+        textAlign: 'center',
+        fontFamily: fonts.complement,
+        color: colors.heading,
+        fontSize: 12,
+        marginBottom: 5
+    }
 })
