79ff41f68eef0bd6ef1ef713faa0d7b2528bb0a5
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-05-16 15:39:23 -0300

    Change the route of plant select to use bottom tab route

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -4,9 +4,9 @@ import colors from '../styles/colors';
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
-import { PlantSelect } from '../pages/PlantSelect';
 import { PlantSave } from '../pages/PlantSave';
 import { MyPlants } from '../pages/MyPlants';
+import AuthRoutes from './tab.routes';
 
 const stackRoutes = createStackNavigator();
 
@@ -36,7 +36,7 @@ const AppRoutes: React.FC = () => (
 
         < stackRoutes.Screen
             name="PlantSelect"
-            component={PlantSelect}
+            component={AuthRoutes}
         />
 
         < stackRoutes.Screen
