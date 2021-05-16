4a5a01c42b2aac601d91a956e70e46a3ffefb318
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-05-16 15:24:18 -0300

    Correct subtitle text

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -46,7 +46,7 @@ export function UserIdentification() {
             await AsyncStorage.setItem('@plantmanager:user', name);
             navigation.navigate('Confirmation', {
                 title: 'Prontinho',
-                subtitle: "Agora vamos começar a cuidar das suas{'\n'}plantinhas com muito cuidado.",
+                subtitle: "Agora vamos começar a cuidar das suas\nplantinhas com muito cuidado.",
                 button: 'Começar',
                 icon: 'smile',
                 nextScreen: 'PlantSelect'
