a752a3eb70b861a5ff3b496856576f16f4fec309
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-05-16 10:48:55 -0300

    Include My Plants route

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -6,6 +6,7 @@ import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
 import { PlantSelect } from '../pages/PlantSelect';
 import { PlantSave } from '../pages/PlantSave';
+import { MyPlants } from '../pages/MyPlants';
 
 const stackRoutes = createStackNavigator();
 
@@ -41,6 +42,11 @@ const AppRoutes: React.FC = () => (
         < stackRoutes.Screen
             name="PlantSave"
             component={PlantSave}
+        />
+
+        < stackRoutes.Screen
+            name="MyPlants"
+            component={MyPlants}
         />
     </stackRoutes.Navigator>
 )
