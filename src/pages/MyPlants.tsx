0c7ff97d6b241c9914f7c2f44083a007b3e8f500
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-05-18 18:43:45 -0300

    include load animation

--- src/pages/MyPlants.tsx
+++ src/pages/MyPlants.tsx
@@ -14,6 +14,7 @@ import { pt } from 'date-fns/locale';
 import { FlatList } from 'react-native-gesture-handler';
 import fonts from '../styles/fonts';
 import { PlantCardSecondary } from '../components/PlantCardSecondary';
+import { Load } from '../components/Load';
 
 
 export function MyPlants() {
@@ -43,6 +44,10 @@ export function MyPlants() {
         loadStorageData();
 
     })
+
+    if (loading) {
+        return < Load />
+    }
 
     return (
         < View style={styles.container}>
