cb54fc9349d7e9d4a2ea295842e718b0fc5d5559
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-05-18 18:48:51 -0300

    Including hour property

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -2,16 +2,17 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { format } from 'date-fns';
 
 export interface PlantProps {
-    id: string,
-    name: string,
-    about: string,
-    water_tips: string,
-    photo: string,
-    environments: [string],
+    id: string;
+    name: string;
+    about: string;
+    water_tips: string;
+    photo: string;
+    environments: [string];
     frequency: {
-        times: number,
-        repeat_every: string
-    },
+        times: number;
+        repeat_every: string;
+    };
+    hour: string;
     dateTimeNotification: Date;
 }
 
