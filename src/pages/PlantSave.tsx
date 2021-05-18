c7b410432ed4c06b483b4868c65cf8601d81011e
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2021-05-18 18:45:31 -0300

    Includ scrowview

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -73,64 +73,69 @@ export function PlantSave() {
     }
 
     return (
-        <View style={styles.container}>
-            <View style={styles.plantInfo}>
-                <SvgFromUri
-                    uri={plant.photo}
-                    height={150}
-                    width={150}
-                />
-
-                <Text style={styles.plantName}>
-                    {plant.name}
-                </Text>
-
-                <Text style={styles.plantAbout}>
-                    {plant.about}
-                </Text>
-            </View>
-            <View style={styles.controller}>
-                <View style={styles.tipContainer}>
-                    <Image
-                        source={waterdrop}
-                        style={styles.tipImage}
+        <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={styles.container}
+        >
+            <View style={styles.container}>
+                <View style={styles.plantInfo}>
+                    <SvgFromUri
+                        uri={plant.photo}
+                        height={150}
+                        width={150}
                     />
-                    <Text style={styles.tipText}>
-                        {plant.water_tips}
+
+                    <Text style={styles.plantName}>
+                        {plant.name}
+                    </Text>
+
+                    <Text style={styles.plantAbout}>
+                        {plant.about}
                     </Text>
                 </View>
-                <Text style={styles.alertLabel}>
-                    Escolha o melhor horário para ser lembrado:
+                <View style={styles.controller}>
+                    <View style={styles.tipContainer}>
+                        <Image
+                            source={waterdrop}
+                            style={styles.tipImage}
+                        />
+                        <Text style={styles.tipText}>
+                            {plant.water_tips}
+                        </Text>
+                    </View>
+                    <Text style={styles.alertLabel}>
+                        Escolha o melhor horário para ser lembrado:
                 </Text>
 
-                {showDatePicker && (
-                    <DateTimePicker
-                        value={selectedDateTime}
-                        mode="time"
-                        display="spinner"
-                        onChange={handleChangeTime}
-                    />)
-                }
+                    {showDatePicker && (
+                        <DateTimePicker
+                            value={selectedDateTime}
+                            mode="time"
+                            display="spinner"
+                            onChange={handleChangeTime}
+                        />)
+                    }
 
-                {
-                    Platform.OS === 'android' && (
-                        <TouchableOpacity
-                            onPress={handleOpenDateTimePickerAndroid}
-                            style={styles.dateTimePickerButton}
-                        >
-                            <Text style={styles.dateTimePickerText}>
-                                {`${format(selectedDateTime, 'HH')} horas ${format(selectedDateTime, 'mm')} min`}
-                            </Text>
-                        </TouchableOpacity>
-                    )
-                }
+                    {
+                        Platform.OS === 'android' && (
+                            <TouchableOpacity
+                                onPress={handleOpenDateTimePickerAndroid}
+                                style={styles.dateTimePickerButton}
+                            >
+                                <Text style={styles.dateTimePickerText}>
+                                    {`${format(selectedDateTime, 'HH')} horas ${format(selectedDateTime, 'mm')} min`}
+                                </Text>
+                            </TouchableOpacity>
+                        )
+                    }
 
-                <Button
-                    title="Cadastrar planta"
-                    onPress={handleSave}
-                />
+                    <Button
+                        title="Cadastrar planta"
+                        onPress={handleSave}
+                    />
+                </View>
             </View>
-        </View>
+        </ScrollView>
     )
 }
 const styles = StyleSheet.create({
